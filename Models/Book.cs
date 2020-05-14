218f71f1c0fd30c350838cdcf837b4ed7336638e
Nate Sherfick <natesherfick@gmail.com>
2020-05-13 21:59:26 -0600

    Book Model

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace console_library.Models
+{
+public class Book
+{
+  public string Title {get; private set;}
+  public string Author {get; private set;}
+  public bool Available {get; private set;}
+}
+}
